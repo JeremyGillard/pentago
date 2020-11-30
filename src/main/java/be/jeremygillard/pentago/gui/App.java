6e2bba893b63c9c36a2fd56c80e7ba2f921e50cf
JeremyGillard <gillard.jeremy@outlook.fr>
2020-11-30 18:11:21 +0100

    Sound game enable because of git lfs - V2.0

--- src/main/java/be/jeremygillard/pentago/gui/App.java
+++ src/main/java/be/jeremygillard/pentago/gui/App.java
@@ -17,9 +17,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         //Atmosphere work for more immersion in the game.
-        // Commented because the wav file was too large for a remote. 
-        //cf: remote: error: GH001: Large files detected. You may want to try Git Large File Storage - https://git-lfs.github.com    
-        //initSurroundingMusic();
+        initSurroundingMusic();
         
         Pentago pentago = new Pentago();
         
@@ -42,8 +40,8 @@ public class App extends Application {
     }
     
     public void initSurroundingMusic() {
-        AudioClip sound = new AudioClip("file:media/sound/Jazz.wav");
-        sound.setVolume(0.1);
+        AudioClip sound = new AudioClip("file:media/sound/jazz.wav");
+        sound.setVolume(0.4);
         sound.play();
     }
     
